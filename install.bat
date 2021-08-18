c5792363260830c767c206ebd91cd28a8f0ae63f
Mark Rai <markraidc@gmail.com>
2021-08-18 10:23:57 -0400

    fixed jigger

--- install.bat
+++ install.bat
@@ -1,5 +1,6 @@
 set /p id="Enter Windows User ID:"
 
 tar xzvf c:exe\exe.zip -C C:exe\
+del c:exe\exe.zip
 copy c:bat\*.* c:\dev\env\
 copy c:exe\*.* "c:\Users\%id%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
